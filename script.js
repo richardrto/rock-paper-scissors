ee5852e8cabc8a0e00785eddcc9cb5a479a89aca
Richard To <31784865+richardrto@users.noreply.github.com>
2024-04-21 11:24:19 +0900

    Create getPlayerSelection & playRound functions
    Creating getPlayerSelection separately allows
    playRound to focus on logic concerning the two
    parameters, playerSelection and computerSelection.
    
    playRound is only one instance of a round, and
    can be repeated elsewhere in the code to be played
    any number of times.

--- script.js
+++ script.js
@@ -2,13 +2,28 @@ function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
     switch (num) {
         case 0:
-            return 'Rock';
+            return 'rock';
             break;
         case 1:
-            return 'Paper';
+            return 'paper';
             break;
         case 2:
-            return 'Scissors';
+            return 'scissors';
             break;
     }
+}
+
+function getPlayerSelection() {
+    return prompt('Enter your choice:');
+}
+
+function playRound(playerSelection, computerSelection) {
+    return playerSelection == computerSelection ? 'It\'s a tie!'
+        : playerSelection == 'rock' && computerSelection == 'paper' ? 'Computer wins!'
+        : playerSelection == 'rock' && computerSelection == 'scissors' ? 'Player wins!'
+        : playerSelection == 'paper' && computerSelection == 'rock' ? 'Player wins!'
+        : playerSelection == 'paper' && computerSelection == 'scissors' ? 'Computer wins!'
+        : playerSelection == 'scissors' && computerSelection == 'rock' ? 'Computer wins!'
+        : playerSelection == 'scissors' && computerSelection == 'paper' ? 'Player wins!'
+        : 'Hm, this is a strange outcome. Try again.';
 }
